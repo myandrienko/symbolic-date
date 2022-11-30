652af30a371c1e711c6eb9b222648a19114e4ca8
Matvey Andrienko <matvey.andrienko@osome.com>
2022-11-30 15:01:00 +0300

    initial commit

---
+++ src/SymbolicDate.test.ts
@@ -0,0 +1,71 @@
+import { it, expect, beforeAll, vi, describe } from "vitest";
+import { reviveSymbolicDate, SymbolicDate } from "./SymbolicDate";
+
+beforeAll(() => {
+  expect(
+    new Date().toString(),
+    "Tests are expected to run in UTC+04:00 timezone. " +
+      "Set TZ environment variable, e.g. TZ=Asia/Baku. " +
+      "Avoid timezones with DST"
+  ).toContain("GMT+0400");
+});
+
+describe("core", () => {
+  it("constructs current date", () => {
+    vi.setSystemTime(new Date(Date.UTC(2022, 11, 1, 1, 0, 0, 0)));
+    const date = new SymbolicDate();
+    expect(date.toUTCString()).toBe("2022-12-01");
+  });
+
+  it("constructs date from ISO string", () => {
+    const date = new SymbolicDate("2022-12-01");
+    expect(date.toUTCString()).toBe("2022-12-01");
+  });
+
+  it("constructs invalid date from invalid ISO date string", () => {
+    const date = new SymbolicDate("2022-12-01T12:00:00.000Z");
+    expect(+date).toBe(Number.NaN);
+  });
+
+  it("constructs date from parts", () => {
+    const date = new SymbolicDate(2022, 11, 1);
+    expect(date.toUTCString()).toBe("2022-12-01");
+  });
+
+  it("can reinterpret Date in local timezone", () => {
+    const date = SymbolicDate.fromLocalDate(new Date(2022, 11, 1, 1, 0, 0, 0));
+    expect(date.toUTCString()).toBe("2022-12-01");
+  });
+
+  it("can reinterpret Date in UTC", () => {
+    const date = SymbolicDate.fromUTCDate(
+      new Date(Date.UTC(2022, 11, 1, 1, 0, 0, 0))
+    );
+    expect(date.toUTCString()).toBe("2022-12-01");
+  });
+
+  it("allows date manipulation", () => {
+    const date = new SymbolicDate("2022-12-01");
+    date.setDate(date.getDate() - 1);
+    date.setFullYear(1970);
+    expect(date.toUTCString()).toBe("1970-11-30");
+  });
+});
+
+describe("JSON", () => {
+  it("correctly serializes to JSON", () => {
+    const date = new SymbolicDate("2022-12-01");
+    expect(JSON.stringify({ date })).toBe('{"date":"2022-12-01"}');
+  });
+
+  it("can be used as JSON reviver", () => {
+    const obj = JSON.parse(
+      '{"date":"2022-12-01","notDate":"20221201"}',
+      reviveSymbolicDate
+    );
+    expect(obj).toStrictEqual({
+      date: new SymbolicDate("2022-12-01"),
+      notDate: "20221201",
+    });
+  });
+});
---
+++ src/SymbolicDate.ts
@@ -0,0 +1,144 @@
+const isoDateRegex = /^\d{4}-[01]\d-[0-3]\d$/;
+const isoDateLength = 10;
+
+export class SymbolicDate {
+  // UTC midnight at the current date
+  private date: Date;
+
+  constructor();
+  constructor(dateString: string);
+  constructor(year: number, monthIndex: number, day: number);
+  constructor(
+    maybeYearOrDateString?: string | number,
+    maybeMonthIndex?: number,
+    maybeDay?: number
+  ) {
+    if (
+      typeof maybeYearOrDateString === "undefined" &&
+      typeof maybeMonthIndex === "undefined" &&
+      typeof maybeDay === "undefined"
+    ) {
+      this.date = new Date();
+      this.date.setUTCHours(0, 0, 0, 0);
+      return this;
+    }
+
+    if (
+      typeof maybeYearOrDateString === "string" &&
+      isoDateRegex.test(maybeYearOrDateString)
+    ) {
+      // Constructing a date from ISO date string produces UTC midnight
+      // timestamp which is exactly what we need
+      this.date = new Date(maybeYearOrDateString);
+      return this;
+    }
+
+    if (
+      typeof maybeYearOrDateString === "number" &&
+      typeof maybeMonthIndex === "number" &&
+      typeof maybeDay === "number"
+    ) {
+      this.date = new Date(
+        Date.UTC(maybeYearOrDateString, maybeMonthIndex, maybeDay)
+      );
+      return this;
+    }
+
+    this.date = new Date(Number.NaN);
+  }
+
+  static fromLocalDate(date: Date): SymbolicDate {
+    return new SymbolicDate(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate()
+    );
+  }
+
+  static fromUTCDate(date: Date): SymbolicDate {
+    return new SymbolicDate(
+      date.getUTCFullYear(),
+      date.getUTCMonth(),
+      date.getUTCDate()
+    );
+  }
+
+  getDate(): number {
+    return this.date.getUTCDate();
+  }
+
+  getMonth(): number {
+    return this.date.getUTCMonth();
+  }
+
+  getFullYear(): number {
+    return this.date.getUTCFullYear();
+  }
+
+  getDay(): number {
+    return this.date.getUTCDay();
+  }
+
+  setDate(date: number): SymbolicDate {
+    this.date.setUTCDate(date);
+    return this;
+  }
+
+  setMonth(monthIndex: number, date?: number): SymbolicDate {
+    this.date.setUTCMonth(monthIndex, date ?? this.date.getUTCDate());
+    return this;
+  }
+
+  setFullYear(year: number, monthIndex?: number, date?: number): SymbolicDate {
+    this.date.setUTCFullYear(
+      year,
+      monthIndex ?? this.date.getUTCMonth(),
+      date ?? this.date.getUTCDate()
+    );
+    return this;
+  }
+
+  toString(): string {
+    return this.date.toDateString();
+  }
+
+  toLocaleString(): string;
+  toLocaleString(locales: string | string[]): string;
+  toLocaleString(
+    locales: string | string[],
+    options: Intl.DateTimeFormatOptions
+  ): string;
+  toLocaleString(
+    locales?: string | string[],
+    options?: Intl.DateTimeFormatOptions
+  ): string {
+    return this.date.toLocaleDateString(locales, options);
+  }
+
+  toUTCString(): string {
+    return this.date.toISOString().substring(0, isoDateLength);
+  }
+
+  [Symbol.toPrimitive](): number {
+    return +this.date;
+  }
+
+  valueOf(): number {
+    return +this.date;
+  }
+
+  toJSON(): string {
+    return this.toUTCString();
+  }
+}
+
+export function reviveSymbolicDate(key: string, value: unknown): unknown {
+  if (typeof value === "string") {
+    const maybeValidDate = new SymbolicDate(value);
+    if (!Number.isNaN(+maybeValidDate)) {
+      return maybeValidDate;
+    }
+  }
+
+  return value;
+}
